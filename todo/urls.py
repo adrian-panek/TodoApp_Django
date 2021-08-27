dc92ad9e01082456b6a1a4e7aed9c992dee577c2
adrian-panek <adrian.panek999@gmail.com>
2021-08-27 22:04:41 +0200

    added some basic styling using bootstrap 4

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,7 @@ app_name = "main_todo"
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="homepage"),
+    path('create/', AddTaskView.as_view(), name="add-task"),
     path('<int:pk>/update', UpdateTaskView.as_view(), name="update-task"),
     path('<int:pk>/delete', DeleteTaskView.as_view(), name="delete-task")
 ]
--- todo/views.py
+++ todo/views.py
@@ -10,6 +10,24 @@ class HomePageView(generic.CreateView, generic.ListView):
     form_class = TaskModelForm
     context_object_name = "tasks"
 
+    def get_queryset(self):
+       query = self.request.GET.get('search')
+       if query:
+          postresult = Task.objects.filter(description__contains=query)
+          result = postresult
+       else:
+           result = None
+       result = super(HomePageView, self).get_queryset()
+       return result
+
+    def get_success_url(self):
+        return reverse("main_todo:homepage")
+
+class AddTaskView(generic.CreateView):
+    template_name = "task_add.html"
+    queryset = Task.objects.all()
+    form_class = TaskModelForm
+
     def get_success_url(self):
         return reverse("main_todo:homepage")
 
