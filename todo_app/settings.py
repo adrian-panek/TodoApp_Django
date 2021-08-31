f651b5adee2caef339f76453ab0310d1f204a66f
adrian-panek <adrian.panek999@gmail.com>
2021-08-31 12:45:11 +0200

    styled app, first final version

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -112,3 +112,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = "static_root"
