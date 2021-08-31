f5ad41bb34315dd3cf3b21775d143efab6f54dac
adrian-panek <adrian.panek999@gmail.com>
2021-08-31 19:04:20 +0200

    added search bar

--- todo/views.py
+++ todo/views.py
@@ -10,15 +10,12 @@ class HomePageView(generic.CreateView, generic.ListView):
     form_class = TaskModelForm
     context_object_name = "tasks"
 
-    def get_queryset(self):
-       query = self.request.GET.get('search')
-       if query:
-          postresult = Task.objects.filter(description__contains=query)
-          result = postresult
-       else:
-           result = None
-       result = super(HomePageView, self).get_queryset()
-       return result
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(description__contains=search_input)
+        return context
 
     def get_success_url(self):
         return reverse("main_todo:homepage")
